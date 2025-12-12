a1d70822c5709a1446740fb811e3949ea92ac665
charlestoastcat-coder <charles.toastcat@gmail.com>
2025-12-11 17:08:22 -0700

    Add files via upload
    Pickles Benchmark

---
+++ constants.ts
@@ -0,0 +1,4 @@
+
+export const NORMAL_TEST_DURATION_MS = 60000; // 1 minute
+export const EXTREME_TEST_DURATION_MS = 20 * 60 * 1000; // 20 minutes
+export const MAX_PARTICLES = 200000; // Increased significantly to ensure device failure at extreme levels
